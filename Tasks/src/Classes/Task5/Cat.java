96afdda42485bb99290543b823c411dcafe1eaed
MaskIra <55229323+MaskIra@users.noreply.github.com>
2020-02-15 19:25:54 +0300

    Add files via upload

---
+++ Tasks/src/Classes/Task5/Cat.java
@@ -0,0 +1,11 @@
+package Classes.Task5;
+
+public class Cat extends Animal {
+    public Cat(){
+        super(200, 2, 0);
+    }
+
+    public Cat(double runMax, double jumpMax, double swimMax){
+        super(runMax, jumpMax, swimMax);
+    }
+}
