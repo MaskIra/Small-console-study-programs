e3d5a79c2d302b74102c24f6237c0ccf22970d66
MaskIra <55229323+MaskIra@users.noreply.github.com>
2020-02-15 20:50:48 +0300

    Add files via upload

--- Tasks/src/Task3.java
+++ Tasks/src/Task3.java
@@ -1,12 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-        tryToKnowNumber.go();
-        tryToKnowWord.go();
+        Scanner scanner = new Scanner(System.in);
+        boolean trigger = true;
+        while (trigger) {
+            System.out.println("What game do you want? 1 – tryToKnowNumber / 2 - tryToKnowWord / 0 – exit");
+            int x = scanner.nextInt();
+            if (x == 0) {
+                trigger = false;
+            } else if (x == 1) {
+                tryToKnowNumber.go();
+            } else if (x == 2) {
+                tryToKnowWord.go();
+            } else {
+                System.out.println("Okay... Try again");
+            }
+        }
     }
 
     static class tryToKnowNumber {
@@ -16,19 +32,24 @@ public class Task3 {
             // компьютер должен сообщить больше ли указанное пользователем число, чем загаданное,
             // или меньше. После победы или проигрыша выводится запрос – «Повторить игру еще раз?
             // 1 – да / 0 – нет»(1 – повторить, 0 – нет).
-            System.out.println("#1 tryToKnowNumber");
+            System.out.println("#1 tryToKnowNumber \n// The computer created a number from 0 to 9 inclusive. " +
+                    "\n// You have 3 attempts to guess it.");
+            Scanner scanner = new Scanner(System.in);
             boolean trigger = true;
             while (trigger) {
                 int number = new Random().nextInt(10);
                 for (int i = 1; i < 4; i++) {
                     System.out.print(i + ") Enter the number: ");
-                    int trying = Integer.parseInt(readInput());
-                    if (number == trying)
+                    int trying = scanner.nextInt();
+                    if (number == trying) {
                         break;
-                    else System.out.println("Oooh! Number is " + ((number > trying) ? "larger" : "less"));
+                    } else {
+                        System.out.println("Oooh! Number is " + ((number > trying) ? "larger" : "less"));
+                    }
                 }
                 System.out.println("You win!\n Do you want try again? 1 – yes / 0 – no");
-                if (Integer.parseInt(readInput()) == 0) trigger = false;
+                int x = scanner.nextInt();
+                if (x == 0) trigger = false;
             }
         }
     }
@@ -48,12 +69,14 @@ public class Task3 {
             String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli",
                     "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom",
                     "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
-            System.out.println("#2 tryToKnowWord");
+            System.out.println("#2 tryToKnowWord \n // Try to guess which word from the following list the computer created\n" +
+                    Arrays.toString(words));
+            Scanner scanner = new Scanner(System.in);
             String word = words[new Random().nextInt(words.length)];
             char[] result = "###############".toCharArray();
             while (true) {
-                System.out.print("- Enter the word (to 15 symbols): ");
-                String trying = readInput().trim();
+                System.out.print("- Enter the word: ");
+                String trying = scanner.next();
                 if (word.equals(trying))
                     break;
                 else {
@@ -64,18 +87,7 @@ public class Task3 {
                     System.out.print("No! But... " + new String(result));
                 }
             }
-            System.out.print("You win!");
+            System.out.println("You win! ");
         }
-    }
-
-    private static String readInput() {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String str = null;
-        try {
-            str = reader.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return str;
     }
 }
